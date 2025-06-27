154ea195c8bd1fc9cebb4c17eea3156d37fe6043
Cristian Vacacela <cvacacelaprocel@cifpfbmoll.eu>
2025-06-27 19:01:38 +0200

    Added: pseudo test of Players interaction

---
+++ test/PlayerInteractionTest.java
@@ -0,0 +1,29 @@
+package test;
+
+import model.*;
+import enums.*;
+import utils.*;
+import static utils.BoatsGenerator.generateBoatsList;
+
+
+public class PlayerInteractionTest {
+    public static void main(String[] args) {
+        Difficulty difficulty = Difficulty.EASY;
+
+
+
+        Board board1 = new Board(difficulty.getBoardSize(), difficulty.getTotalSize(), generateBoatsList(difficulty));
+        board1.placeShips();
+        Board board2 = new Board(difficulty.getBoardSize(), difficulty.getTotalSize(), generateBoatsList(difficulty));
+        board2.placeShips();
+
+        HumanPlayer Juan = new HumanPlayer("Juan", board1, difficulty.getAttempts());
+        CpuPlayer robotito = new CpuPlayer("Robotito", board2, difficulty.getAttempts());
+
+        Juan.nextShot(robotito.getBoard());
+
+
+
+
+    }
+}
