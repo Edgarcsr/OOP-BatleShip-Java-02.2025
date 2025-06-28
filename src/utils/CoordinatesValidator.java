a7906ef1ca722c41fba4c8033de69a386e25a671
Cristian Vacacela <cvacacelaprocel@cifpfbmoll.eu>
2025-06-28 18:22:17 +0200

    Fixed: wrong validates on uses cases

--- src/utils/CoordinatesValidator.java
+++ src/utils/CoordinatesValidator.java
@@ -1,39 +1,53 @@
 package utils;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class CoordinatesValidator {
 
-    public static int checkRow(Scanner scanner) {
+    public static int checkRow(Scanner scanner, String message) {
+        System.out.printf("%s", message);
 
-        if(scanner.nextLine().trim().isEmpty()) {
-            System.out.println("Error: No puedes dejar la coordenada vacia.");
-            return checkRow(scanner);
+        int row;
+        String inputTemporary = scanner.nextLine().trim();
+
+        if (inputTemporary.isEmpty()) {
+            System.out.println("No puedes dejar la coordenada vacía. Por favor, introduce un número entero válido para la fila.");
+            System.out.println();
+            return checkRow(scanner, "Introduce la fila (0-9): ");
+        } else {
+            try {
+                row = Integer.parseInt(inputTemporary);
+            } catch (NumberFormatException e) {
+                System.out.println();
+                System.out.println("Error: Debes introducir un número entero válido para la fila. ");
+                return checkRow(scanner, "Introduce la fila (0-9): ");
+            }
         }
-
-        int row = Integer.parseInt(scanner.nextLine().trim());
-
-        if(row >= 0 && row <= 9) {
-            return row;
-        }
-        System.out.println("Error: Debes introducir un número entero válido para la fila.");
-        return checkRow(scanner);
+        return row >= 0 && row <= 9 ? row : checkRow(scanner, "No seas pirata, introduce un número entre 0 y 9: ");
     }
 
-    public static int checkColumn(Scanner scanner) {
+    public static int checkColumn(Scanner scanner, String message) {
+        System.out.printf("%s", message);
 
-        if(scanner.nextLine().trim().isEmpty()) {
-            System.out.println("Error: No puedes dejar la coordenada vacia.");
-            return checkColumn(scanner);
+        String temporaryInput = scanner.nextLine().trim();
+        String column;
+
+        if (temporaryInput.isEmpty()) {
+            System.out.println("Error: No puedes dejar la coordenada vacía.");
+            System.out.println();
+            return checkColumn(scanner, "Introduce la columna (A-J): ");
+        } else if (temporaryInput.matches("\\d+|[!\\|·#$~%&/()=?¿'{¨}.,:;_]")) {
+            System.out.println("Error: Numeros o carácteres especiales no son válidos.");
+            System.out.println();
+            return checkColumn(scanner, "Introduce la columna (A-J): ");
         }
 
-        String column = scanner.nextLine().trim().toUpperCase();
+        column = temporaryInput.toUpperCase();
 
-        if(column.length() == 1 && column.charAt(0) >= 'A' && column.charAt(0) <= 'J') {
-            return column.charAt(0) - 'A';
-        }
-        System.out.println("Error: Debes introducir una letra válida para la columna (A-J).");
-        return checkColumn(scanner);
+        return (column.length() == 1 && column.charAt(0) >= 'A' && column.charAt(0) <= 'J') ? column.charAt(0) - 'A' : checkColumn(scanner, "No seas pirata, introduce solo una letra entre A y J: ");
     }
 }
 
