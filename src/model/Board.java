4643ff0004a962aaff6289071f374ea0be215017
Cristian Vacacela <cvacacelaprocel@cifpfbmoll.eu>
2025-07-11 17:21:33 +0200

    Fixed: warnings

--- src/model/Board.java
+++ src/model/Board.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 public class Board {
     private final Cell[][] grid;
-    private List<Ship> ships;
+    private final List<Ship> ships;
     private final RandomCellGenerator randomCellGenerator;
     final private List<String> missedShoots = new ArrayList<>();
     final private List<String> successfullyShots = new ArrayList<>();
