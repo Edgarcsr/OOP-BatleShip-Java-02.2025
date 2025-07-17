e7d5462d4b3718e3ec90749d8dc74550c6ec03d5
Cristian Vacacela <cvacacelaprocel@cifpfbmoll.eu>
2025-07-17 19:17:08 +0200

    Change: refactoring name of methods and variables. Added: reset method

--- src/model/Cell.java
+++ src/model/Cell.java
@@ -6,21 +6,18 @@ import enums.ShipType;
 
 
 public class Cell {
-
     private Ship ship;
     private  CellStatus status;
     private final Coordinate coordinate;
 
     public Cell( Coordinate coordinates ) {
-
         this.ship = null;
         this.status = CellStatus.EMPTY;
         this.coordinate = coordinates;
     }
 
     public void processHit( Coordinate coords ) {
-
-        if(status == CellStatus.EMPTY) {
+        if (status == CellStatus.EMPTY) {
             status = CellStatus.MISS;
         } else if( status == CellStatus.SHIP) {
             ship.registryHitAt(coords);
@@ -33,7 +30,6 @@ public class Cell {
     }
 
     public void setShip(Ship ship) {
-
         this.ship = ship;
         this.status = CellStatus.SHIP;
     }
@@ -51,25 +47,26 @@ public class Cell {
     }
 
     public String display(boolean reveal) {
-
-        if(status == CellStatus.HIT) {
+        if (status == CellStatus.HIT) {
             return "X";
-        } else if(status == CellStatus.MISS) {
+        } else if (status == CellStatus.MISS) {
             return "A";
-        } else if(reveal && status == CellStatus.SHIP) {
+        } else if (reveal && status == CellStatus.SHIP) {
             return switch(ship.getType()) {
-                case ShipType.LANCHA -> "L";
-                case ShipType.BUQUE -> "B";
-                case ShipType.ACORAZADO -> "Z";
-                case ShipType.PORTAVIONES -> "P";
+                case ShipType.BOAT -> "L";
+                case ShipType.SHIP -> "B";
+                case ShipType.BATTLESHIP -> "Z";
+                case ShipType.AIRCRAFTCARRIER -> "P";
             };
-        }else {
+        } else {
             return "-";
         }
     }
 
     protected void reset() {
-        this.ship = null;
-        this.status = CellStatus.EMPTY;
+        switch (status) {
+            case MISS -> this.status = CellStatus.EMPTY;
+            case HIT -> this.status =  ship != null ? CellStatus.SHIP : CellStatus.EMPTY;
+        }
     }
 }
